d03d17206e30f6de360b1a012b32151ae3b7eeee
dgbarros <117647212+dgbarros@users.noreply.github.com>
2024-01-31 18:58:14 -0300

    upgrade js

--- form.js
+++ form.js
@@ -1,6 +1,33 @@
 class FormSubmit{
     constructor(settings) {
         this.settings = settings;
-        this.form = document
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute('action');
+        }
+    }
+
+    displaySuccess(){
+        this.form.innerHtml = this.settings.success;
+    }
+    
+    displayError() {
+        this.form.innerHtml = this.settings.error;
+    }
+
+    init() {
+        if (this.form) this.formButton.addEventListenner("click", () => this.displaySuccess());
+        return this;
     }
 }
+
+const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Mensagem NÃO enviada!</h1>",
+})
+
+
+formSubmit.init();
